1917c3c9d1ba736f1fdcc82565cc67284d528fb9
Natali Duque <nduquev8@gmail.com>
2020-03-25 11:57:41 +0000

    Âinitial commit Covid-19 tracker

---
+++ generate_plots.py
@@ -0,0 +1,65 @@
+import cufflinks as cf
+from plotly.offline import init_notebook_mode
+import os
+import pandas as pd
+cf.go_offline()
+
+init_notebook_mode(connected=True)
+
+homedir = "/home/alarm/corona"
+#homedir = "/home/christian/Downloads/"
+os.chdir(homedir)
+git_repo = os.path.join(homedir, "COVID-19")
+if os.path.exists(git_repo):
+    os.chdir(git_repo)
+    os.system("git pull")
+    os.chdir(homedir)
+else:
+    os.system("git clone https://github.com/CSSEGISandData/COVID-19")
+
+
+_map = {"Cape Verde": "Cabo Verde",
+        "Czech Republic": 'Czechia',
+        "South Korea": "Korea, South",
+        "Taiwan": "Taiwan*", 
+        "United States": "US"}
+
+pop = pd.read_csv("app/population.csv")[["name","pop2019"]]
+pop.replace(_map,inplace=True)
+pop.index = pop.name
+del pop["name"]
+pop.pop2019*=1000
+
+
+confirmed_global = "COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+df = pd.read_csv(confirmed_global)
+df = df.groupby("Country/Region").sum()
+df.drop(labels=["Lat", "Long"],axis=1, inplace= True)
+df = df.transpose()
+df.index = pd.to_datetime(df.index, format="%m/%d/%y")
+
+_exclude = ['Congo (Brazzaville)', 
+            'Congo (Kinshasa)', 
+            "Cote d'Ivoire", 
+            'Cruise Ship',
+            'Eswatini', 
+            "Holy See",
+            'North Macedonia']
+
+df.drop(labels=_exclude, axis=1, inplace=True)
+
+normed = pd.DataFrame()
+for column in df.columns:
+    normed[column] =df[column].astype(float)/pop.loc[column][0]
+
+    
+def gen_plots(df, name):
+    df.iplot(kind="bar",filename = "app/plots/{}".format(name), asUrl=True)
+    countries = ["Colombia", "Germany", "Italy", "Switzerland", "US"]
+    # countries = df.columns.tolist() # in case you want all countries as single plots
+    for country in countries:
+        df[country].iplot(kind="bar",filename = "app/plots/{}_{}".format(country.replace(" ","_").lower(), name), asUrl=True)
+
+        
+gen_plots(df, "raw")
+gen_plots(normed, "norm")
---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import os
+
+homedir = "/home/alarm/corona/app"
+#homedir = "/home/christian/Downloads"
+os.chdir(homedir)
+
+app = Flask(__name__, template_folder="plots")
+
+@app.route("/")
+def serve():
+    return render_template("norm.html")
+
+@app.route("/<key>")
+def serve_key(key):
+    try:
+        return render_template("{}.html".format(key))
+    except:
+        return "<h1>No plot to this shortcut available.</h1><h3>You may try 'norm', 'raw' or a country name in connection with 'norm' or 'raw' eg 'switzerland_raw'</h3>"
+
+app.run(host="0.0.0.0",port=5000)
