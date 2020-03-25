19b421e66b12d3ff91c5c060eac095e1ef675bbf
Natalia Duque <nduquev8@gmail.com>
2020-03-25 21:26:33 +0000

    debug server on

--- server.py
+++ server.py
@@ -18,4 +18,4 @@ def serve_key(key):
     except:
         return "<h1>No plot to this shortcut available.</h1><h3>You may try 'norm', 'raw' or a country name in connection with 'norm' or 'raw' eg 'switzerland_raw'</h3>"
 
-app.run(host="0.0.0.0",port=5000)
+app.run(host="0.0.0.0",port=5000,debug=True)
