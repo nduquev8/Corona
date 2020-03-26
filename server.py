48d0c09bc04da31abcc4400c5ab2235f3aa2b131
Natalia Duque <nduquev8@gmail.com>
2020-03-26 15:20:20 +0000

    fixed server

--- server.py
+++ server.py
@@ -16,6 +16,6 @@ def serve_key(key):
     try:
         return render_template("{}.html".format(key))
     except:
-        return "<h1>No plot to this shortcut available.</h1><h3>Available endpoints are:\n'{}'.</h3>".format("', '".join([os.path.splitext(n) for n in os.listdir("plots")]))
+        return "<h1>No plot to this shortcut available.</h1><h3>Available endpoints are:\n'{}'.</h3>".format("', '".join([os.path.splitext(n)[0] for n in os.listdir("plots")]))
 
 app.run(host="0.0.0.0",port=5000,debug=True)
