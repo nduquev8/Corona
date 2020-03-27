3dacb337e6d9613315220e01f364fb80b385a961
Christian Förster <christian.foerster@eawag.ch>
2020-03-27 20:41:37 +0100

    changed dropbox_api

--- dropbox_api.py
+++ dropbox_api.py
@@ -7,22 +7,24 @@ def update_on_dropbox():
 
     dbx = dropbox.Dropbox(token)
     dbx.users_get_current_account()
+    
+    dropbox_folder = "/Corona_Statistics"
 
-    def send(dbx, file):
+    def send(dbx, path, file):
         with open(file,"rb") as f:
-            return dbx.files_upload(f.read(), '/{}'.format(os.path.split(file)[-1]))
-    def ls(dbx):
-        return [f.name for f in dbx.files_list_folder("").entries]
+            return dbx.files_upload(f.read(), os.path.join(path, os.path.split(file)[-1]))
+        
+    def ls(dbx, path):
+        return [f.name for f in dbx.files_list_folder(path).entries]
 
-    def delete(dbx,name):
-        return dbx.files_delete("/"+name)
+    def delete(dbx, path, name):
+        return dbx.files_delete(os.path.join(path,name))
 
-
-    current_files = ls(dbx)
+    current_files = ls(dbx,dropbox_folder)
     for file in current_files:
-        delete(dbx, file)
+        delete(dbx, dropbox_folder, file)
 
     for file in os.listdir("app_corona/plots"):
-        send(dbx, "app_corona/plots/"+file)
+        send(dbx, dropbox_folder, "app_corona/plots/"+file)
     
     
