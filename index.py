90b6a62e13ff799da67dd9c42c6ebb0614bebae8
Christian Förster <christian.foerster@eawag.ch>
2020-03-29 13:29:48 +0200

    new index_api

--- index.py
+++ index.py
@@ -49,145 +49,153 @@ def add_navbar_to_plots(folder="app_corona/plots"):
             with open(os.path.join(folder,html),"w") as f:
                 f.write(content)
     
+def link(name, ref):
+     return '<u><a href="{ref}" class="text-dark alert-link">{name}</a></u>'.format(**locals())
+ 
+def entry(text, **links):
+    return text.format(**links)+"<br/>"
+
+def alert(typ, title, entries):
+    """
+    typ like on https://getbootstrap.com/docs/4.4/components/alerts/
+    eg.:
+        info
+        warning
+        danger
+        success
+        ...
+    """
+    
+    alert="""
+    <div class="alert alert-{typ}" role="alert">
+    <h4 class="alert-heading text-dark"><b>{title}</b></h4>
+      <p>
+      {entry_section}
+      </p>
+    </div>
+    """.format(typ=typ,
+               title=title, 
+               entry_section="\n".join(entries))
+    return alert
+
+def page(title, alerts):
+    alert_section = "\n".join(alerts)
+    page="""
+    <!doctype html>
+    <html lang="en">
+      <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+      <title>{title}</title>
+      </head>
+      <body>
+
+        <nav class="navbar navbar-light bg-light navbar-right">
+        <a class="navbar-brand" href="#">Corona Statistics</a>
+            <form class="form-inline my-2 my-lg-0">
+            </form>
+
+          <form class="form-inline my-2 my-lg-0">
+            <a class="btn btn-outline-secondary mr-sm-2" href="index_es.html" role="button">Espa&ntildeol</a>
+            <a class="btn btn-outline-success my-2 my-sm-0" href="index_en.html" role="button">English</a>
+          </form>
+        </nav>
+                
+        <div class="pt-5 container">
+
+        <h1>{title}</h1>
+        <br/>
+        {alert_section}
+        
+        </div>
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+      </body>
+    </html>
+    """.format(title=title, alert_section=alert_section)
+    return page
+
+def save_page(page, path):
+    with open(path,"w") as html:
+        html.write(page)
 
 def generate_index():
     
-    ### ADD FILES AND LINK TITLES FOR ES HERE
-    file_to_es = {
-        'all.html': 'Total',
-        'raw.html': 'Total',
-        'raws.html': 'Datos agregados',
-        'norm.html': 'Proporcio&#769n',
-        'norms.html': 'Datos agregados',
-        'ratec.html': 'Tasa de crecimiento diario',
-        'ratec_heatmap.html': 'Mapa de calor normalizado',
-        'death.html': 'Total',
-        'rated.html': 'Tasa de crecimiento diario'
-    }
-    ### ADD FILES AND LINK TITLES FOR ENG HERE
-    file_to_en = {
-        'all.html': 'Total',
-        'raw.html': 'Total',
-        'raws.html': 'Aggregated data',
-        'norm.html': 'Portion',
-        'norms.html': 'Aggregated data',
-        'ratec.html': 'Daily growth rate',
-        'ratec_heatmap.html': 'Normalized heat map',
-        'death.html': 'Total',
-        'rated.html': 'Daily growth rate'
-    }
-        
-    hyperlink_format = '<u><a href="{link}" class="text-dark alert-link">{text}</a></u>'
+    ### SPANISH
+    l1 = link('Total', 'all.html')
+    l2 = link('Total', 'raw.html')
+    l3 = link('Datos agregados', 'raws.html')
+    l4 = link('Proporcio&#769n', 'norm.html')
+    l5 = link('Datos agregados', 'norms.html')
+    l6 = link('Tasa de crecimiento diario', 'ratec.html')
+    l7 = link('Mapa de calor normalizado', 'ratec_heatmap.html')
+    l8 = link('Total', 'death.html')
+    l9 = link('Tasa de crecimiento diario', 'rated.html')
+
+    e1 = entry("1. {link} de contagios por pais.", 
+               link=l1)
+    e2 = entry("2. {link1} de contagios en paises de intere&#769s. ({link2})", 
+               link1=l2, link2=l3)
+    e3 = entry("3. {link1} de contagios en paises de intere&#769s de acuerdo con la poblacio&#769n.({link2})", 
+               link1=l4, link2=l5)
+    e4 = entry("4. {link1} de contagios en paises de intere&#769s.({link2})", 
+               link1=l6, link2=l7)
+    e5 = entry("1. {link1} de muertes en paises de intere&#769s.", 
+               link1=l8)
+    e6 = entry("2. {link1} de muertes en paises de intere&#769s.", 
+               link1=l9)
+
+    a1=alert("warning", "Contagios", 
+             [e1, e2, e3, e4])
+    a2=alert("danger", "Muertes", 
+             [e5, e6])
+
+    p_es=page("Estadi&#769sticas COVID-19", 
+           [a1, a2])
+
+    save_page(p_es, "app_corona/plots/index_es.html")
+
+    ### ENGLISH
+    l1 = link('Total', 'all.html')
+    l2 = link('Total', 'raw.html')
+    l3 = link('Aggregated data', 'raws.html')
+    l4 = link('Portion', 'norm.html')
+    l5 = link('Aggregated data', 'norms.html')
+    l6 = link('Daily growth rate', 'ratec.html')
+    l7 = link('Normalized heat map', 'ratec_heatmap.html')
+    l8 = link('Total', 'death.html')
+    l9 = link('Daily growth rate', 'rated.html')
+
+    e1 = entry("1. {link1} number of infections by country.", 
+               link1=l1)
+    e2 = entry("2. {link1} number of infections in countries of interest. ({link2})", 
+               link1=l2, link2=l3)
+    e3 = entry("3. {link1} of infections in countries of interest according to population.({link2})", 
+               link1=l4, link2=l5)
+    e4 = entry("4. {link1} of infections in countries of interest.({link2})", 
+               link1=l6, link2=l7)
+    e5 = entry("1. {link1} deaths in countries of interest.", 
+               link1=l8)
+    e6 = entry("2. {link1} of deaths in countries of interest.", 
+               link1=l9)
+
+    a1=alert("warning", "Confirmed", 
+             [e1, e2, e3, e4])
+    a2=alert("danger", "Deaths", 
+             [e5, e6])
+
+    p_en=page("COVID-19 Statistics", 
+           [a1, a2])
+
+    save_page(p_en, "app_corona/plots/index_en.html")
     
-    languages = [
-        ### ADD TEXT FOR ES HERE
-        """<title>Estadisticas COVID-19</title>
-              </head>
-              <body>
-              
-                <nav class="navbar navbar-light bg-light navbar-right">
-                <a class="navbar-brand" href="#">Corona Statistics</a>
-                    <form class="form-inline my-2 my-lg-0">
-                    </form>
-
-                  <form class="form-inline my-2 my-lg-0">
-                    <a class="btn btn-outline-success mr-sm-2" href="index_es.html" role="button">Espa&ntildeol</a>
-                    <a class="btn btn-outline-secondary my-2 my-sm-0" href="index_en.html" role="button">English</a>
-                  </form>
-                </nav>
-              
-                <div class="pt-5 container">
-
-                    <h1>Estadi&#769sticas COVID-19</h1>
-                    <br/>
-                    <div class="alert alert-warning" role="alert">
-                    <h4 class="alert-heading text-dark"><b>Contagios</b></h4>
-                      <p>1. {} de contagios por pais.<br/>
-                      2. {} de contagios en paises de intere&#769s. ({})<br/>
-                      3. {} de contagios en paises de intere&#769s de acuerdo con la poblacio&#769n.({})<br/>
-                      4. {} de contagios en paises de intere&#769s.({})<br/></p>
-                      <br/>
-                    </div>
-                    <div class="alert alert-danger" role="alert">
-                    <h4 class="alert-heading text-dark"><b>Muertes</b></h4>
-                      <p>1. {} de muertes en paises de intere&#769s.<br/>
-                      2. {} de muertes en paises de intere&#769s.<br/></p>
-
-                    </div>
-                </div>""".format(*[hyperlink_format.format(link=key, text=val) for key,val in file_to_es.items()]),
-                
-        ### ADD TEXT FOR ENG HERE
-        """<title>COVID-19 Statistics</title>
-              </head>
-              <body>
-              
-                <nav class="navbar navbar-light bg-light navbar-right">
-                <a class="navbar-brand" href="#">Corona Statistics</a>
-                    <form class="form-inline my-2 my-lg-0">
-                    </form>
-
-                  <form class="form-inline my-2 my-lg-0">
-                    <a class="btn btn-outline-secondary mr-sm-2" href="index_es.html" role="button">Espa&ntildeol</a>
-                    <a class="btn btn-outline-success my-2 my-sm-0" href="index_en.html" role="button">English</a>
-                  </form>
-                </nav>
-              
-              
-                <div class="pt-5 container">
-
-                    <h1>COVID-19 Statistics</h1>
-                    <br/>
-                    <div class="alert alert-warning" role="alert">
-                    <h4 class="alert-heading text-dark"><b>Confirmed Cases</b></h4>
-                      <p>1. {} number of infections by country.<br/>
-                      2. {} number of infections in countries of interest. ({})<br/>
-                      3. {} of infections in countries of interest according to population.({})<br/>
-                      4. {} of infections in countries of interest.({})<br/></p>
-                      <br/>
-                    </div>
-                    <div class="alert alert-danger" role="alert">
-                    <h4 class="alert-heading text-dark"><b>Deaths</b></h4>
-                      <p>1. {} deaths in countries of interest.<br/>
-                      2. {} of deaths in countries of interest.<br/></p>
-
-                    </div>
-                </div>""".format(*[hyperlink_format.format(link=key, text=val) for key,val in file_to_en.items()])
-    ]
-
-    index = []
-    for language in languages:
-        index.append(
-            
-            """
-            <!doctype html>
-            <html lang="en">
-              <head>
-                <!-- Required meta tags -->
-                <meta charset="utf-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-                <!-- Bootstrap CSS -->
-                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-                {}
-                <!-- Optional JavaScript -->
-                <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-                <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-                <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-              </body>
-            </html>
-            """.format(language)
-        )
-
-
-
-    # write html index file for es
-    with open(r"app_corona/plots/index_es.html", "w") as file:
-        file.write(index[0])
-        
-    # write html index file for eng
-    with open(r"app_corona/plots/index_en.html", "w") as file:
-        file.write(index[1])
     
     add_navbar_to_plots()
